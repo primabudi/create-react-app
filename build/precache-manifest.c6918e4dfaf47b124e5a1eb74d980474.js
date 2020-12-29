71556a7bfa496f39a11dd5880a9b7d1963349f58
primabudi-shopee <andi.primabudi@shopee.com>
2020-12-29 13:13:53 +0700

    cra

---
+++ build/precache-manifest.c6918e4dfaf47b124e5a1eb74d980474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c01d36a74bb158b3e8245075d5e4de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bbbb26c63ca884e19f0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0eb7d123f2ee315b1e48",
+    "url": "/static/js/2.f590bbd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f590bbd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbbb26c63ca884e19f0",
+    "url": "/static/js/main.ae1a63d9.chunk.js"
+  },
+  {
+    "revision": "eba815bdb75a30246acb",
+    "url": "/static/js/runtime-main.19e7c3c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
